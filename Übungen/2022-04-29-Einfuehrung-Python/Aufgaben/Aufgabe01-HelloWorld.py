fefb0b7fcab4063d249291126eee2897bc34aa3e
Jonas Pleyer <jonas.pleyer@fdm.uni-freiburg.de>
2022-04-26 18:11:51 +0200

    Erste Ideen für interaktive Aufgaben.

---
+++ Übungen/2022-04-29-Einfuehrung-Python/Aufgaben/Aufgabe01-HelloWorld.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+if __name__ == "__main__":
+    # 01-01
+    # Schreibe ein Programm, das
+    # den folgenden Satz ausgibt:
+    # "Ich finde programmieren ganz arg toll!"
+    #
+    #
+    # 01-02
+    # Erweitere das Programm so, dass der Satz 5mal ausgegeben wird.
+    #
+    #
+    # 01-03
+    # Führe eine Variable ein, die den Satz enthält, den wir ausgeben wollen
---
+++ Übungen/2022-04-29-Einfuehrung-Python/Aufgaben/Aufgabe02-Rechenaufgaben.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+if __name__ == "__main__":
+    # 02-01 Berechne 4^3
+    #
+    #
+    # 02-02 Berechne Wurzel(2)
+    #
+    #
+    # 02-03
+    # Charlie hat 400 Melonen und will damit auf dem Marks überleben
+    # Man braucht pro Tag 2.3 Melonen um genügend Nahrung zu bekommen
+    # Nach {50,100,150} Tagen steigt der Verbrauch auf 2.6 Melonen pro Tag an.
+    # Wie viele Tage überlebt Charlie?
+    #
+    #
+    # 02-04
+    # Die Oberfläche von Hackfleisch ist ein ausgezeichneter Nährboden
+    # Luca hat eine wunderschöne Hackfleischkugel mit 400g Veganem Hack
+    # und einem Durchmesser von 10cm geformt und möchte
+    # wissen, um wie viel sich die Oberfläche und damit die für Bakterien
+    # relevante Fläche vergrößert, wenn er weitere 300g hinzufügt.
