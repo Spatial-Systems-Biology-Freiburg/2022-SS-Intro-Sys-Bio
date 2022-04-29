9abfc620de35e6109aa0c977d3e759c21dc0a04c
Jonas Pleyer <jonas.pleyer@fdm.uni-freiburg.de>
2022-04-29 12:50:30 +0200

    added final tasks

---
+++ Übungen/2022-04-29-Einfuehrung-Python/Aufgaben/Aufgaben05-Plotting.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+
+if __name__ == "__main__":
+	# 05-01
+    # Füge eine geeignete library hinzu, um numerische berechnungen zu machen
+
+    # 05-02
+    # Finde im Internet heraus, wie man eine zufallszahl mit dieser library generiert
+    # und stecke diese dann in eine variable
+
+    # 05-03
+    # Finde heraus, wie man Listen/arrays mit dieser library erstellt
+    # und erstelle eine solche mit zahlen zwischen 0 und 10
+
+    # 05-04
+    # Berechne mit dieser library cosinus, sinus und tangens der zuvor generierten werte
+    # und speichere die ergebnisse in verschiedenen listen
+
+    # 05-05
+    # Finde eine library, um die Ergebnisse zu plotten und mache das für alle 3 ergebnisse (in die gleiche Grafik)
+
+    # 05-06
+    # Finde im Internet heraus:
+    # Füge titel, skalenbenennung und Kurvenbenennung hinzu
+
+    # 05-07
+    # Wie kann ich alle kurven in verschiedenen "subplots" darstellen?
+    # Inklusive titel, skalenbenennung + kurvenbenennung
+
+    # 05-08
+    # Mache das gleiche für Ergebnisse der Liste von Aufgabe03
+    # Automatisiere das für beliebige Listen dieser Struktur
+    pass
