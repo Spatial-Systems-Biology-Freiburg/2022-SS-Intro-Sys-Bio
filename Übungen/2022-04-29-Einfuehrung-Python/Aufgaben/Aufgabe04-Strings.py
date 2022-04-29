56e8f30233856fb8725dc19558a0478fa9cd675d
Jonas Pleyer <jonas.pleyer@fdm.uni-freiburg.de>
2022-04-29 12:43:31 +0200

    added aufgaben for string manipulation.

---
+++ Übungen/2022-04-29-Einfuehrung-Python/Aufgaben/Aufgabe04-Strings.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+if __name__ == "__main__":
+	# 04-01
+	# Definiere zwei strings deiner Wahl und verkette sie zu einem
+
+	# 04-02
+	# Nimm die beiden strings von zuvor und teile sie an einem Zeichen auf, das in beiden strings vorkommt
+
+	# 04-03
+	# Gibt den letzten buchstaben von jedem Wort aus.
+
+	# 04-04
+	woerter = ["hallo, das hier sollen einfach ein paar ganz ganz komische und klein-geschriebene Wörter sein. Mal schauen, was so passiert!"]
+	# Ersetze den Anfangsbuchstaben von jedem Wort durch einen Großuchstaben
