c5bb3b8d8d9b68bb1c67bedd53533672f93d08df
Jonas P <jonaspleyer@gmx.de>
2022-06-21 21:39:57 +0200

    refined solution for one working parameter set. also implement nice output.

--- Projekte/Code/example.py
+++ Projekte/Code/example.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-from pde_functions import jpat, full_model
+from pde_functions import jpat
 from pde_int import couplingMatrix, IJKth
 from save_results import save_plots
 from scipy.integrate import solve_ivp
@@ -13,12 +13,36 @@ def random_initialiser(xmax, ymax, NVar):
     return np.random.normal(loc=1, scale=0.1, size=xmax*ymax*NVar)
 
 
+def full_model(t, y, D, ind, k, start_time):
+    print("[{: >8.4f}] Solving ...".format(time.time()-start_time), end="\r")
+    dydt = np.zeros(y.shape)
+    TTG1 = y[ind]
+    GL1  = y[ind+1]
+    GL3  = y[ind+2]
+    TRY  = y[ind+3]
+    CPC  = y[ind+4]
+    AC1  = y[ind+5]
+    AC2  = y[ind+6]
+    dydt[ind]   = k[0] - TTG1*(k[1] + k[2]*GL3) + (k[1]*k[3])*np.dot(D,TTG1)
+    dydt[ind+1] = k[4] + k[5]*AC2 - GL1*(k[6] + k[7]*GL3)
+    dydt[ind+2] = k[8] + (k[22]*k[10]*AC2*AC2)/(k[22]+AC2*AC2) \
+                - GL3*(k[11] + k[7]*GL1 + k[13]*CPC) \
+                + (k[23]*k[9]*AC1*AC1)/(k[23]+AC1*AC1) \
+                - k[2]*TTG1*GL3 - k[12]*TRY*GL3
+    dydt[ind+3] = k[14]*AC1*AC1 - TRY*k[15] - TRY*GL3*k[12] + k[15]*k[16]*np.dot(D,TRY)
+    dydt[ind+4] = k[17]*AC2*AC2 - k[18]*CPC - k[13]*CPC*GL3 + k[18]*k[19]*np.dot(D,CPC)
+    dydt[ind+5] = k[2]*GL3*TTG1 - k[20]*AC1
+    dydt[ind+6] = k[7]*GL3*GL1 - k[21]*AC2
+    return dydt
+
+
+
 if __name__ == "__main__":
-    xmax=10
-    ymax=10
+    xmax=15
+    ymax=15
     NVar=7
     t_span = (0, 1000)
-    t_num = 50
+    t_num = 200
     y0 = random_initialiser(xmax, ymax, NVar)
     bndcondition="zeroflux"
     celltype="quadratic"
@@ -37,7 +61,7 @@ if __name__ == "__main__":
     start_time = time.time()
     print("[{: >8.4f}] Solving ...".format(0), end="\r")
     sol = solve_ivp(
-        lambda t, y: full_model(t, y, D, ind, k),
+        lambda t, y: full_model(t, y, D, ind, k, start_time),
         t_span,
         y0,
         method='Radau',
