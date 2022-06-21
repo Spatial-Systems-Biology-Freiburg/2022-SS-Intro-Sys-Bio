f4621b1a8252b85893ce3d4abccb5ebfa1ebaf8b
Jonas P <jonaspleyer@gmx.de>
2022-06-21 11:51:16 +0200

    try procedure with big system.

--- Projekte/Code/reaction_diffusion_system_example.py
+++ Projekte/Code/reaction_diffusion_system_example.py
@@ -15,9 +15,30 @@ def ODE(X, t):
     return (k1 - k2*A + k3*A**2*B, k4 - k3*A**2*B)
 
 
+k = [0.5982, 0.1405, 2.1971, 1.1245, 0.2916, 2.3028, 0.3466, 1.7822, 0.3976, 9.9829,\
+        1.2590, 2.6202, 1.5731, 5.2625, 4.8758, 0.3196, 0.1465, 2.1453, 0.5396, 56.0520,\
+        0.5131, 0.8396, 7.8041, 1.3647]
+
+
+def full_model(X, t):
+	dydt = np.zeros(X.shape)
+	(TTG1, GL1, GL3, TRY, CPC, AC1, AC2) = X
+	dydt[0]   = k[0] - TTG1*(k[1] + k[2]*GL3)
+	dydt[0+1] = k[4] + k[5]*AC2 - GL1*(k[6] + k[7]*GL3)
+	dydt[0+2] = k[8] + (k[22]*k[10]*AC2*AC2)/(k[22]+AC2*AC2) \
+                - GL3*(k[11] + k[7]*GL1 + k[13]*CPC) \
+                + (k[23]*k[9]*AC1*AC1)/(k[23]+AC1*AC1) \
+                - k[2]*TTG1*GL3 - k[12]*TRY*GL3
+	dydt[0+3] = k[14]*AC1*AC1 - TRY*k[15] - TRY*GL3*k[12]
+	dydt[0+4] = k[17]*AC2*AC2 - k[18]*CPC - k[13]*CPC*GL3
+	dydt[0+5] = k[2]*GL3*TTG1 - k[20]*AC1
+	dydt[0+6] = k[7]*GL3*GL1 - k[21]*AC2
+	return dydt
+
+
 if __name__ == "__main__":
     # initial_values = np.random.uniform(low=200.0, high= 900.0, size=(2, 10, 10))
-    initial_values = np.zeros((2, 15, 15))
+    initial_values = np.zeros((7, 30, 30))
     initial_values += 900.0
     for i in range(1, 9):
         initial_values[:,i,i] = 950.0
@@ -29,8 +50,8 @@ if __name__ == "__main__":
         initial_values=initial_values,
         boundary_type="neumann",
         boundary_values=0.0,
-        diffusion_constants=np.array([100.0, 5000.0]),
-        kinetics=ODE
+        diffusion_constants=np.array([50, 0, 0, 100, 5000, 0, 0]),
+        kinetics=full_model
     )
 
     res = pde_solv.solve_pde(np.arange(0, 7200, 0.1))
