d3548d917c09ab44740e46385118e9d50c57255d
Jonas P <jonaspleyer@gmx.de>
2022-06-21 21:34:50 +0200

    Revert "implemented schnakenberg model with working parameter set."
    This reverts commit 057576a4178f1125b055d8c4f467f68da029d109.

--- Projekte/Code/example.py
+++ Projekte/Code/example.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-from pde_functions import jpat
+from pde_functions import jpat, full_model
 from pde_int import couplingMatrix, IJKth
 from save_results import save_plots
 from scipy.integrate import solve_ivp
@@ -9,39 +9,35 @@ import numpy as np
 import time
 
 
-def ODE(t, y, D, ind, k, start_time):
-    print("[{: >8.4f}] Solving ...".format(time.time()-start_time), end="\r")
-    dydt = np.zeros(y.shape)
-    A = y[ind+0]
-    B = y[ind+1]
-    dydt[ind]   = k[0] - k[1]*A + k[2]*A**2*B   + k[3]*np.dot(D,A)
-    dydt[ind+1] = k[4] - k[2]*A**2*B            + k[5]*np.dot(D,B)
-    return dydt
+def random_initialiser(xmax, ymax, NVar):
+    return np.random.normal(loc=1, scale=0.1, size=xmax*ymax*NVar)
 
 
 if __name__ == "__main__":
-    xmax=30
-    ymax=30
-    NVar=2
-    t_span = (0, 5000)
-    t_num = 1000
-    y0 = np.random.uniform(low=600, high=800, size=xmax*ymax*NVar)
+    xmax=10
+    ymax=10
+    NVar=7
+    t_span = (0, 1000)
+    t_num = 50
+    y0 = random_initialiser(xmax, ymax, NVar)
     bndcondition="zeroflux"
     celltype="quadratic"
 
     k = [
-        10.0, 0.1, 4.83e-07, 100.0/40**2,
-        80.0, 5000.0/40**2
+        0.5982, 0.1405, 2.1971, 1.1245, 0.2916, 2.3028, 0.3466, 1.7822, 0.3976, 9.9829,
+        1.2590, 2.6202, 1.5731, 5.2625, 4.8758, 0.3196, 0.1465, 2.1453, 0.5396, 56.0520,
+        0.5131, 0.8396, 7.8041, 1.3647
     ]
 
     t_eval = np.linspace(t_span[0], t_span[1], t_num)
+
     D=couplingMatrix(xmax, ymax, bndcondition, celltype)
     ind = IJKth(1, np.arange(ymax), np.arange(xmax), ymax, NVar)
 
     start_time = time.time()
     print("[{: >8.4f}] Solving ...".format(0), end="\r")
     sol = solve_ivp(
-        lambda t, y: ODE(t, y, D, ind, k, start_time),
+        lambda t, y: full_model(t, y, D, ind, k),
         t_span,
         y0,
         method='Radau',
@@ -52,6 +48,6 @@ if __name__ == "__main__":
     res = sol.y.reshape((xmax, ymax, NVar, len(t_eval)))
     print("[{: >8.4f}s] Solving Done".format(time.time()-start_time))
 
-    component_index = 0
-    step = 5
+    component_index = 1
+    step = 1
     save_plots(res, component_index, step)
