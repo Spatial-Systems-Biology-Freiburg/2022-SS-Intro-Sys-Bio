44e99ad500ab893c5129512dc775c7014a5f9877
Jonas Pleyer <jonas.pleyer@fdm.uni-freiburg.de>
2022-06-28 17:55:58 +0200

    include examples for simple neural network with pytorch.

---
+++ Vorlesung/2022-06-29-Machine-Learning/examples/test.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+
+import torch
+from train import NeuralNetwork
+from torchvision import datasets
+from torchvision.transforms import ToTensor
+
+if __name__ == "__main__":
+    # Download test data from open datasets.
+    test_data = datasets.FashionMNIST(
+        root="data",
+        train=False,
+        download=True,
+        transform=ToTensor(),
+    )
+
+    model = NeuralNetwork()
+    model.load_state_dict(torch.load("model.pth"))
+
+    classes = [
+        "T-shirt/top",
+        "Trouser",
+        "Pullover",
+        "Dress",
+        "Coat",
+        "Sandal",
+        "Shirt",
+        "Sneaker",
+        "Bag",
+        "Ankle boot",
+    ]
+
+    model.eval()
+    x, y = test_data[0][0], test_data[0][1]
+    print(test_data[0][1])
+    with torch.no_grad():
+        pred = model(x)
+        predicted, actual = classes[pred[0].argmax(0)], classes[y]
+        print(f'Predicted: "{predicted}", Actual: "{actual}"')
